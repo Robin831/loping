f15fba78191e694a2270e687a84d3ec96c21fe3c
Kevin Smith <ksm@uc.no>
2022-06-08 17:27:18 +0200

    Popup --> Signinredirect

--- src/Laktat.tsx
+++ src/Laktat.tsx
@@ -1,42 +1,53 @@
-import { FC, ReactElement, useState } from "react";
-import {  db, auth, provider } from "./firebaseSetup"
-import { addDoc, collection, getDocs } from "firebase/firestore"; 
-import { signInWithPopup } from "firebase/auth";
+import { FC, ReactElement, useEffect, useState } from "react";
+import { db, auth, provider } from "./firebaseSetup"
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { signInWithRedirect } from "firebase/auth";
 import LaktatChart from "./Components/LaktatChart";
 import { Fart, Maling, Puls } from "./Models/maling";
 import styled from 'styled-components';
-  
+
 const Container = styled.div`
     display: flex;
     justify-content: center;
     align-content: center;
 `;
 
-const Laktat: FC = (): ReactElement =>  {
+const Laktat: FC = (): ReactElement => {
     const [bruker, setBruker] = useState<string | null | undefined>(auth.currentUser?.uid);
     const [lineData, setLineData] = useState<any[]>([]);
     const [malinger, setMalinger] = useState<Maling[]>([]);
 
+    useEffect(() => {
+        checkAuthState();
+    }, []);
+
+    const checkAuthState = () => {
+        auth.onAuthStateChanged(user => {
+            if (user) {
+                setBruker(user.uid)
+            } else {
+                setBruker(null);
+            }
+        })
+    }
+
     const googleSignIn = async () => {
-        const result = await signInWithPopup(auth, provider);
-        const user = result.user;
-        setBruker(user.uid);
-      }
-    
+        await signInWithRedirect(auth, provider);
+    }
+
     const fetchData = async () => {
-        if (bruker)
-        {
+        if (bruker) {
             const data = await getDocs(collection(db, "laktat", bruker, "malinger"));
-   
+
             let malingList: Maling[] = [];
 
             data.forEach((doc) => {
                 let laktat = doc.data();
                 let maling = new Maling();
                 maling.id = doc.id;
-                
 
-                let d: any[] = [];                
+
+                let d: any[] = [];
 
                 laktat.data.forEach((l: any) => {
                     let data = {
@@ -44,7 +55,7 @@ const Laktat: FC = (): ReactElement =>  {
                         laktat: l.verdi
                     }
                     d.push(data);
-                    
+
                     let puls = new Puls();
                     let fart = new Fart();
 
@@ -54,7 +65,7 @@ const Laktat: FC = (): ReactElement =>  {
                     fart.fart = l.fart;
 
                     maling.pulsDataMalinger.push(puls);
-                    maling.fartDataMalinger.push(fart);                    
+                    maling.fartDataMalinger.push(fart);
                 });
 
                 malingList.push(maling);
@@ -67,67 +78,67 @@ const Laktat: FC = (): ReactElement =>  {
 
             setMalinger(malingList);
         }
-        
-    } 
+
+    }
 
     const setData = async () => {
         if (bruker) {
-            await addDoc(collection(db, "laktat", bruker, "malinger"), 
-            {
-                data: [
-                    {
-                        fart: 13,
-                        puls: 140,
-                        verdi: 5.3,
-                    },
-                    {
-                        fart: 14,
-                        puls: 150,
-                        verdi: 6.3,
-                    }
+            await addDoc(collection(db, "laktat", bruker, "malinger"),
+                {
+                    data: [
+                        {
+                            fart: 13,
+                            puls: 140,
+                            verdi: 5.3,
+                        },
+                        {
+                            fart: 14,
+                            puls: 150,
+                            verdi: 6.3,
+                        }
 
-                ]
-            });
+                    ]
+                });
         }
-    } 
+    }
 
-  
+
     return (
         <header className="App-header">
-        {!bruker && 
-            <button onClick={googleSignIn}>Sign in with Google</button>
-        }
-        {bruker && 
-            <>
-            <p>Terskel</p>
-            <button onClick={fetchData}>Hent data</button>
-            <button onClick={setData}>Sett data</button>
-            <Container>
-                    {malinger.length > 0 && malinger.map( ma => (
-                        <LaktatChart 
-                        data={ma.fartDataMalinger} 
-                        syncId={ma.id}
-                        xKey="fart"
-                        lineKey="laktat"
-                        xDomain={[11, 15]}
-                        yDomain={[1, 8]}
-                    />
-                    ))}
-            </Container>
-            <Container>
-                    {malinger.length > 0 && malinger.map( ma => (
-                        <LaktatChart 
-                        data={ma.pulsDataMalinger} 
-                        syncId={ma.id + 'puls'}
-                        xKey="puls"
-                        lineKey="laktat"
-                        xDomain={[130, 190]}
-                        yDomain={[1, 8]}
-                    />
-                    ))}
-            </Container>
-            </>
-        }
+            {!bruker &&
+                <button onClick={googleSignIn}>Sign in with Google</button>
+            }
+            {bruker &&
+                <>
+                    <p>Terskel</p>
+                    <button onClick={fetchData}>Hent data</button>
+                    <button onClick={setData}>Sett data</button>
+                    <Container>
+                        {malinger.length > 0 && malinger.map(ma => (
+                            <LaktatChart
+                                data={ma.fartDataMalinger}
+                                syncId={ma.id}
+                                xKey="fart"
+                                lineKey="laktat"
+                                xDomain={[11, 15]}
+                                yDomain={[1, 8]}
+                            />
+                        ))}
+                    </Container>
+                    <Container>
+                        {malinger.length > 0 && malinger.map(ma => (
+                            <LaktatChart
+                                data={ma.pulsDataMalinger}
+                                syncId={ma.id + 'puls'}
+                                xKey="puls"
+                                lineKey="laktat"
+                                xDomain={[130, 190]}
+                                yDomain={[1, 8]}
+                            />
+                        ))}
+                    </Container>
+                </>
+            }
         </header>
     );
 };
